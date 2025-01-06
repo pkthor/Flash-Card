51c10caa358785e6525b208df2d7f5da0d4a09ea
pkthor <pkthor@gmail.com>
2025-01-06 07:56:16 -0700

    Initial Commit

---
+++ Missionary Flash/Missionary_FlashApp.swift
@@ -0,0 +1,17 @@
+//
+//  Missionary_FlashApp.swift
+//  Missionary Flash
+//
+//  Created by P. Kurt Thorderson on 1/6/25.
+//
+
+import SwiftUI
+
+@main
+struct Missionary_FlashApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
